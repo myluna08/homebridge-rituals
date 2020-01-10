04a904b057c5c2b6b54f7b9e2ed9b64d45394649
golfito <59629971+myluna08@users.noreply.github.com>
2020-01-10 03:41:51 +0100

    Update index.js
    rewrite as fan accessory

--- index.js
+++ index.js
@@ -1,83 +1,111 @@
-var request = require("request");
-var Service, Characteristic;
+'use strict';
+
+const request = require('request');
+const version = require('./package.json').version;
+
+let Service;
+let Characteristic;
+let logger;
+
+var on_state = false;
+var fan_speed = 1;
 
 module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
   
-  homebridge.registerAccessory("homebridge-rituals", "Rituals", RitualsAccessory);
+  homebridge.registerAccessory('homebridge-rituals', 'Rituals', RitualsAccessory);
 }
 
 function RitualsAccessory(log, config) {
   this.log = log;
-  this.name = config["name"];
-  this.account = config["account"];
-  this.password = config["password"];
-  this.token = config["token"];
-  this.hash = config["hash"];
+  this.services = [];
+  this.name = config.name || 'Genie';
+  this.account = config.account;
+  this.password = config.password;
+  this.token = config.token;
+  this.hash = config.hash;
   
-  this.service = new Service.Mechanism(this.name);
-  
+  this.service = new Service.Fan();
   this.service
-    .getCharacteristic(Characteristic.CurrentState)
-    .on('get', this.getState.bind(this));
-  
+  	.getCharacteristic(Characteristic.On)
+  	.on('get', callback => callback(null, on_state))
+  	.on('set', this.setActiveState.bind(this));
+  	
   this.service
-    .getCharacteristic(Characteristic.TargetState)
-    .on('get', this.getState.bind(this))
-    .on('set', this.setState.bind(this));
-}
+  	.getCharacteristic(Characteristic.RotationSpeed)
+  	.setProps({
+        minValue: 1,
+        maxValue: 4
+      })
+  	.on('get', callback => callback(null, fan_speed))
+  	.on('set', this.setFanSpeed.bind(this));
 
-RitualsAccessory.prototype.getState = function(callback) {
-  this.log("Rituals getting current state...");
+  this.serviceInfo = new Service.AccessoryInformation();
+  this.serviceInfo
+  	.setCharacteristic(Characteristic.Manufacturer, 'Rituals')
+  	.setCharacteristic(Characteristic.Model, 'Genie')
+  	.setCharacteristic(Characteristic.SerialNumber, 'SN000000001')
+  	.setCharacteristic(Characteristic.FirmwareRevision, '1.0.0')
+ 
+  this.services.push(this.service);
+  this.services.push(this.serviceInfo);
   
-  request.get({
-    url: "https://rituals.sense-company.com/api/account/hubs/"+this.token,
-  }, function(err, response, body) {
-    
-    if (!err && response.statusCode == 200) {
-      var json = JSON.parse(body);
-      var state = json.hub.attributes.fanc; // "0" or "1" if is stopped or running
-      this.log(" => response is %s", state);
-      callback(null, state); // success
-    }
-    else {
-      this.log("Error getting state (status code %s): %s", response.statusCode, err);
-      callback(err);
-    }
-  }.bind(this));
-}
-  
-RitualsAccessory.prototype.setState = function(state, callback) {
-  var genieState = (state == "0") ? "1" : "0";
-
-  this.log("Set Rituals state to %s", genieState);
-  var str = '{"hub": "'+this.hash+'","json": {"attr": {"fanc" : "'+genieState+'"}}}';
-  var jsonData = JSON.parse(str);
-    
-  request.post({
-    url: "https://rituals.sense-company.com/api/hub/update/attr",
-    qs: { body: jsonData }
-  }, function(err, response, body) {
-
-    if (!err && response.statusCode == 200) {
-      this.log("Rituals State change complete.");
-      
-      var json = JSON.parse(body);
-      var responseState = json.hub.attributes.fanc; // "0" or "1" if is stopped or running
-      
-      this.service
-        .setCharacteristic(Characteristic.CurrentState, responseState);
-      
-      callback(null); // success
-    }
-    else {
-      this.log("Error '%s' Genie setting state. Response: %s", err, body);
-      callback(err || new Error("Error setting genie state."));
-    }
-  }.bind(this));
+  this.discover();
 }
 
-RitualsAccessory.prototype.getServices = function() {
-  return [this.service];
-}
+RitualsAccessory.prototype = {
+	
+	discover: function () {
+		const that = this;
+		this.log('** Discover');
+	},
+	
+	setActiveState: function(active, callback){
+		this.log('parse value %s', active)
+		on_state = on_state == true ? false : true;
+		this.log('setting Active state to ' + on_state);
+		callback(undefined, on_state);
+	},
+	
+	setFanSpeed: function(value, callback){
+		this.log('setting Fan Rotator at %s', value);
+		fan_speed = 1;
+		callback(null, fan_speed);
+	},
+		
+	identify: function (callback) {
+		callback();
+	},
+
+	getServices: function () {
+		return this.services;
+	},
+	
+	_doRequest: function (methodName, url, httpMethod, urlName, callback, successCallback) {
+        if (!url) {
+            this.log('Ignoring ' + methodName + '() request, [' + urlName + '] is not defined!');
+            callback(new Error('No [' + urlName + '] defined!'));
+            return;
+        }
+        request({
+            url: url,
+            body: '',
+            method: httpMethod,
+            rejectUnauthorized: false
+        },
+        function (error, response, body) {
+            if (error) {
+                this.log(methodName + '() failed: %s', error.message);
+                callback(error);
+            }
+            else if (response.statusCode !== 200) {
+                this.log(methodName + '() returned http error: %s', response.statusCode);
+                callback(new Error('Got http error code ' + response.statusCode));
+            }
+            else {
+                successCallback(body);
+            }
+        }.bind(this));
+    }
+};
